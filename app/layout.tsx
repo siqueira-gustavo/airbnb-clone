1614c50631c54aa905c9128c5abceec94847911c
Gustavo Siqueira <guss.ns@gmail.com>
2023-08-06 15:02:40 -0300

    Fixing hydration problem with ClientOnly wrapper component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter as Nunito } from 'next/font/google'
+import ClientOnly from './components/ClientOnly'
 import NavBar from './components/navbar/NavBar'
 import './globals.css'
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
-        <NavBar />
+        <ClientOnly>
+          <NavBar />
+        </ClientOnly>
         {children}
       </body>
     </html>
